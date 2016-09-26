7473d2ba70c84b85c658c10513313f13d0da1601
Felipe Sandoval <felipesandoval@MacBookPro-de-Felipe.local>
2016-09-26 12:45:43 +0200

    Corregida segun estilo  pep8

--- calcoo.py
+++ calcoo.py
@@ -4,35 +4,35 @@
 
 import sys
 
+
 class Calculadora:
-	def __init__(self, op1, op2):
-		self.var1 = op1
-		self.var2 = op2
+    def __init__(self, op1, op2):
+        self.var1 = op1
+        self.var2 = op2
 
-	def add(self):
-		return (self.var1 + self.var2)
+    def add(self):
+        return (self.var1 + self.var2)
 
-	def minus(self):
-		return (self.var1 - self.var2)
+    def minus(self):
+        return (self.var1 - self.var2)
 
 
 if __name__ == "__main__":
-	try:
-		operando1 = int(sys.argv[1])
-		operando2 = int(sys.argv[3])
-	except ValueError:
-		sys.exit("Error: Non numerical parameters")
-	except IndexError:
-		sys.exit("Error: You need to give me numbers on the Command Line")
+    try:
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
+    except IndexError:
+        sys.exit("Error: You need to give me numbers on the Command Line")
 
-# making the object	
-	variables = Calculadora(operando1, operando2)
+    variables = Calculadora(operando1, operando2)  # making the object
 
-	if sys.argv[2] == "suma":
-		result = Calculadora.add(variables)
-	elif sys.argv[2] == "resta":
-		result = Calculadora.minus(variables)
-	else:
-		sys.exit('You can only add or subtract.')
+    if sys.argv[2] == "suma":
+        result = Calculadora.add(variables)
+    elif sys.argv[2] == "resta":
+        result = Calculadora.minus(variables)
+    else:
+        sys.exit('You can only add or subtract.')
 
-	print(result)
+    print(result)
