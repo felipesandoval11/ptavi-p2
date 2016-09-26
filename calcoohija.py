b22f050312bd2efd377ae13962875966d43b6443
Felipe Sandoval <felipesandoval@MacBookPro-de-Felipe.local>
2016-09-26 12:45:54 +0200

    Corregida segun estilo  pep8

--- calcoohija.py
+++ calcoohija.py
@@ -5,39 +5,37 @@
 import sys
 import calcoo
 
+
 class CalculadoraHija(calcoo.Calculadora):
-	def multiply(self):
-		return (self.var1 * self.var2)
+    def multiply(self):
+        return (self.var1 * self.var2)
 
-	def divide(self):
-		try:
-			return (self.var1 / self.var2)
-		except ZeroDivisionError:
-			sys.exit("Division by zero is not allowed.")
-
+    def divide(self):
+        try:
+            return (self.var1 / self.var2)
+        except ZeroDivisionError:
+            sys.exit("Division by zero is not allowed.")
 
 if __name__ == "__main__":
-	try:
-		operando1 = int(sys.argv[1])
-		operando2 = int(sys.argv[3])
-	except ValueError:
-		sys.exit("Error: Non numerical parameters")
-	except IndexError:
-		sys.exit("Error: You need to give me numbers on the Command Line")
+    try:
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
+    except IndexError:
+        sys.exit("Error: You need to give me numbers on the Command Line")
 
-# making the object	
-	variables = calcoo.Calculadora(operando1, operando2)
+    variables = calcoo.Calculadora(operando1, operando2)  # making the object
 
-	if sys.argv[2] == "suma":
-		result = CalculadoraHija.add(variables)
-	elif sys.argv[2] == "resta":
-		result = CalculadoraHija.minus(variables)
-	elif sys.argv[2] == "multiplica":
-		result = CalculadoraHija.multiply(variables)
-	elif sys.argv[2] == "divide":
-		result = CalculadoraHija.divide(variables)
-	else:
-		sys.exit('You can only add or subtract.')
+    if sys.argv[2] == "suma":
+        result = CalculadoraHija.add(variables)
+    elif sys.argv[2] == "resta":
+        result = CalculadoraHija.minus(variables)
+    elif sys.argv[2] == "multiplica":
+        result = CalculadoraHija.multiply(variables)
+    elif sys.argv[2] == "divide":
+        result = CalculadoraHija.divide(variables)
+    else:
+        sys.exit('You can only add or subtract.')
 
-
-	print(result)
+    print(result)
